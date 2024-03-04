2edba807c8dd4ef50c57fb80307b8ecba32aff32
NikitaShipovskiy222 <nikita04.shipovskiy@gmail.com>
2024-03-04 08:48:33 +0200

    Initial Commit

---
+++ UISwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISwitch
+//
+//  Created by Nikita Shipovskiy on 04/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
